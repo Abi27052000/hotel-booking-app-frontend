d48fdd348ed605c4c71b2ffa544096a6727d2931
ABINASH A. <allesabinash1@gmail.com>
2024-11-08 19:08:37 +0530

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/layout";
 
 function App() {
  
-
   return (
-    <h1 className="bg-gray-500">edgjfj</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element= {<Layout><p>home page</p></Layout>}></Route>
+        <Route path="/search" element= {<Layout><p>search page</p></Layout>}></Route>
+        <Route path="*" element= {<Navigate to="/"/>}></Route>
+
+      </Routes>
+    </Router>
   )
 }
 
